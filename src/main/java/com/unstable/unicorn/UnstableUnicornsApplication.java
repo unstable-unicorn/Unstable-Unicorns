13d26ec6dc77c04774b8721999618c78b3c96e1e
David Jiang <djiang86@binghamton.edu>
2019-02-12 03:02:33 +0000

    spring boot template

---
+++ src/main/java/com/unstable/unicorn/UnstableUnicornsApplication.java
@@ -0,0 +1,14 @@
+package com.unstable.unicorn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UnstableUnicornsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UnstableUnicornsApplication.class, args);
+	}
+
+}
+
