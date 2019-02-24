221e1a414ecdc08ad6de07bf3f02314aba4c2bd9
David Jiang <djiang86@binghamton.edu>
2019-02-24 10:36:13 -0500

    Using the attributes retrieved from YAML file, create unicorns and store them inside UnicornCards HashMap

--- src/main/java/com/unstable/unicorn/card/CardLibrary.java
+++ src/main/java/com/unstable/unicorn/card/CardLibrary.java
@@ -8,6 +8,7 @@ import org.slf4j.LoggerFactory;
 import java.io.File;
 import java.net.URL;
 import java.util.HashMap;
+import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
 
@@ -27,23 +28,40 @@ public class CardLibrary {
         getAllUnicornCards();
     }
 
+    /**
+     * Retrieve the list of attributes for unicorns from "unicorn-cards.yml" file.
+     * Parse the List of LinkedHashMap. The list is basically the list of unicorns and the LinkedHashMap
+     * contains all of the properties for a single unicorn.
+     * Using the properties retrieved from the HashMap, create a new Unicorn class and store it in our Unicorn Cards.
+     */
     @SuppressWarnings("unchecked")
     private void getAllUnicornCards() {
         ObjectMapper mapper = new ObjectMapper(new YAMLFactory());
         try {
             URL cardYmlFileLocation = CardLibrary.class.getResource("/cards/unicorn-cards.yml");
-            Map<String, List<UnicornCard>> unicornCard = mapper.readValue(new File(cardYmlFileLocation.toURI()), Map.class);
+            Map<String, List<LinkedHashMap<String, String>>> listMap = mapper.readValue(new File(cardYmlFileLocation.toURI()), Map.class);
 
-            for(Map.Entry<String, List<UnicornCard>> entry : unicornCard.entrySet()) {
-                List<UnicornCard> values = entry.getValue();
-                if(values != null) {
-                    for (Object value : values) {
-                        System.out.println(String.valueOf(value));
-                    }
+            for(Map.Entry<String, List<LinkedHashMap<String, String>>> entry : listMap.entrySet()) {
+                List<LinkedHashMap<String, String>> values = entry.getValue();
+                // parse all of the attributes in the HashMap
+                // and store them inside an UnicornCard class, then add the unicorn class to the HashMap
+                for (LinkedHashMap<String, String> attributes : values) {
+                    UnicornCard unicornCard = new UnicornCard();
+                    unicornCard.setId(Integer.parseInt(attributes.get("id")));
+                    unicornCard.setName(attributes.get("name"));
+                    unicornCard.setType(Type.valueOf(attributes.get("type")));
+                    unicornCard.setEffect(attributes.get("effect"));
+                    unicornCard.setDescription(attributes.get("description"));
+                    unicornCard.setBasic(Boolean.valueOf(attributes.get("basic")));
+                    unicornCard.setMagical(Boolean.valueOf(attributes.get("magical")));
+                    unicornCard.setBaby(Boolean.valueOf(attributes.get("baby")));
+
+                    this.unicornCards.put(unicornCard.getId(), unicornCard);
                 }
             }
         } catch (Exception e) {
             logger.error("getAllUnicornCards() -> Failed to retrieve unicorn cards from property file");
+            e.printStackTrace();
         }
     }
 
---
+++ src/main/java/com/unstable/unicorn/utitlity/JSONParser.java
@@ -0,0 +1,4 @@
+package com.unstable.unicorn.utitlity;
+
+public class JSONParser {
+}
